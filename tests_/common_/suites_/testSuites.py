ca84d6106eebeb37d5285edcb48cf668008f17ee
JoMa <jorj@noblescripts.com>
2024-08-08 22:12:47 +0400

    Added configuration files

---
+++ tests_/common_/suites_/testSuites.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class TestSuites:
+    """
+        This class provides methods to create test suites for various testing scenarios, allowing for organized and
+        purpose-driven test case grouping.
+    """
+    def get_regression_suite(self):
+        pass
+
+    def get_smoke_suite(self):
+        pass
+
+    def get_performance_suite(self):
+        pass
+
+    def get_random_suite(self):
+        pass
